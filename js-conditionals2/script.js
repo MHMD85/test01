c2190c090b360b727b3b26dc7fc421d36f2f5b90
mhmd <mohammadi1985@gmail.com>
2020-09-23 09:57:22 +0200

    first commit

---
+++ js-conditionals2/script.js
@@ -0,0 +1,7 @@
+let heightMark = 1.80;
+let heightJohn = 1.87;
+let massMark = 79;
+let massJohn = 135.5;
+let bmiMark = massMark /(heightMark * heightMark);
+let bmiJohn = massJohn / (heightJohn * heightJohn);
+
